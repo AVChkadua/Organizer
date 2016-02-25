12f34b6f3baccbfd9421bcac9ab556a38cedb7e7
AVChkadua <anton60.98@gmail.com>
2016-02-25 21:46:39 +0300

    Заготовка главного окна приложения

--- src/ru/mephi/chkadua/ui/MainWindow.java
+++ src/ru/mephi/chkadua/ui/MainWindow.java
@@ -39,7 +39,6 @@ public final class MainWindow implements Runnable {
         listsPanel.setLayout(new GridLayout(1, 2));
         String[] data = {};
         addList(data, listsPanel);
-        addList(data, listsPanel);
         listsPanel.setPreferredSize(new Dimension(480,280));
         frame.add(listsPanel, BorderLayout.CENTER);
 
